fda40a129c36b6fc00c1bfefde734482cb3f9fb9
Deepak/101094 <deepakpandey101094@gmail.com>
2024-01-09 01:53:54 +0530

    create class for apiError and apiResponse

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// using try & catch method
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//   try {
+//     await fun(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
