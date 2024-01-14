0569ffe039a8586f79c590680e3c9963be88920d
Deepak/101094 <deepakpandey101094@gmail.com>
2024-01-15 02:42:26 +0530

    add auth middleware for logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -161,7 +161,30 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+export { registerUser, loginUser, logoutUser };
 
 // const registerUser = async (req, res) => {
 //   try {
